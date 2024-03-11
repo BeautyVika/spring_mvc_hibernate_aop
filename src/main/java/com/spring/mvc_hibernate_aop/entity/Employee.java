c2e4287cd0b0bfd41244567a2730530722f36d2f
victoria <vikachk54@gmail.com>
2024-03-11 12:59:35 +0300

    add project

---
+++ src/main/java/com/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.spring.mvc_hibernate_aop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private  String surname;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private int salary;
+
+}
