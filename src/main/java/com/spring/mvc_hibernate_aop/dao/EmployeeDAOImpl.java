bd2ca0e3a2e2735d75d4a96662d53c7006d3104b
victoria <vikachk54@gmail.com>
2024-03-12 12:23:34 +0300

    add crud operations

--- src/main/java/com/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -18,9 +18,30 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public List<Employee> getAllEmployees() {
 
         Session session = sessionFactory.getCurrentSession();
-
         List<Employee> allEmployees = session.createQuery("FROM Employee", Employee.class).getResultList();
 
         return allEmployees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("DELETE FROM Employee WHERE id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
